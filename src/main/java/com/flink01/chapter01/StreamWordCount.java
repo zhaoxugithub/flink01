49dcf167332b7132bb1fb7ef58f262066e28a3b1
serenipity <1193131227@qq.com>
2022-07-12 00:21:51 +0800

    提交

--- src/main/java/com/flink01/chapter01/StreamWordCount.java
+++ src/main/java/com/flink01/chapter01/StreamWordCount.java
@@ -1,7 +1,6 @@
 package com.flink01.chapter01;
 
 import org.apache.flink.api.common.typeinfo.Types;
-import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.client.program.StreamContextEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -22,7 +21,7 @@ public class StreamWordCount {
 //            Arrays.stream(line.split(" ")).forEach((word) -> out.collect(word));
             //简化成：
             Arrays.stream(line.split(" ")).forEach(out::collect);
-        }).returns(Types.STRING).map(word -> Tuple2.of(word, 1L)).returns(Types.TUPLE(Types.STRING, Types.LONG));
+        }).setParallelism(4).returns(Types.STRING).map(word -> Tuple2.of(word, 1L)).returns(Types.TUPLE(Types.STRING, Types.LONG));
         KeyedStream<Tuple2<String, Long>, String> tuple2StringKeyedStream = flatMapDSS.keyBy(t -> t.f0);
         SingleOutputStreamOperator<Tuple2<String, Long>> sum = tuple2StringKeyedStream.sum(1);
         sum.print();
---
+++ src/main/java/com/flink01/chapter05/SourceCustomTest.java
@@ -0,0 +1,17 @@
+package com.flink01.chapter05;
+
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class SourceCustomTest {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        //如果是自定义source是实现了SourceFunction，这里的并发度只能为1
+        //如果要多个并发度，需要实现ParallelSourceFunction 接口
+        env.setParallelism(1);
+        //有了自定义的source function，调用addSource方法
+        DataStreamSource<Event> stream = env.addSource(new ClickSource());
+        stream.print("SourceCustom");
+        env.execute();
+    }
+}
